77d59a0e0de6478375dc5d65a3ed4560a3ff7ec9
Evgeniy Golovashev <59750599+golovasheveo@users.noreply.github.com>
2020-07-21 15:17:35 +0300

    Strings Opps

--- 01-dogs-galery/scripts/main.js
+++ 01-dogs-galery/scripts/main.js
@@ -2,14 +2,17 @@ var detailImage = document.querySelector('.detail-image');
 var thumbnails = document.querySelectorAll('a');
 var detailTitle = document.querySelector('.detail-title');
 var detailFrame = document.querySelector('.detail-frame');
-
+var HIDDEN = 'hidden-class';
+var thumbnailsList = document.querySelector('.thumbnails-list')
 function setDetails(thumbnail) {
     detailImage.setAttribute('src', thumbnail.getAttribute('href'));
     detailTitle.textContent = thumbnail.getAttribute('data-detail-title');
 }
 
 function showDetails() {
+    detailFrame.classList.remove(HIDDEN)
     detailFrame.classList.add('is-tiny');
+    thumbnailsList.classList.remove(HIDDEN)
     setTimeout(function () {
         detailFrame.classList.remove('is-tiny');
     }, 0 )
@@ -23,7 +26,11 @@ function addListener(thumbnail) {
         showDetails();
     })
 }
-// thumbnails.forEach(addListener);
-for (var i = 0; i < thumbnails.length; i++) {
-    addListener(thumbnails[i]);
+thumbnails.forEach(addListener);
+// for (var i = 0; i < thumbnails.length; i++) {
+//     addListener(thumbnails[i]);
+// }
+function hideDetails() {
+    detailFrame.classList.add(HIDDEN);
+    thumbnailsList.classList.add(HIDDEN);
 }
