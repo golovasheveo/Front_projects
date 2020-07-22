c302da25c80ae4fb7e599fabe1a545624240e69a
Evgeniy Golovashev <59750599+golovasheveo@users.noreply.github.com>
2020-07-22 11:50:36 +0300

    Strings Opps 22072020

--- 03-strings-operations/index.js
+++ 03-strings-operations/index.js
@@ -1,5 +1,17 @@
 var input1 = document.getElementById('input1');
-var result = document.getElementById('result')
+var result = document.getElementById('result');
+var resultTitle = document.getElementById('result-title')
 function displayResult() {
+    resultTitle.hidden = false;
     result.textContent = eval(input1.value);
+
+}
+function reset() {
+    input1.value = '';
+    resultTitle.hidden = true;
+    result.textContent = '';
+}
+
+function stringLength() {
+
 }
