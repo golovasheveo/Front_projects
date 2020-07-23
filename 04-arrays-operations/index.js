178d3f3ffee7b09653edc9d56994bef774171389
Evgeniy Golovashev <59750599+golovasheveo@users.noreply.github.com>
2020-07-23 11:30:44 +0300

    Array add 2307 1130

--- 04-arrays-operations/index.js
+++ 04-arrays-operations/index.js
@@ -9,17 +9,51 @@ function createRandomArray(array) {
 
 createRandomArray(array);
 //adding
+// console.log('-'.repeat(50));
+// console.log('Adding operations');
+// console.log('array before adding: ', array.toString());;
+// //adding at beginning
+// array.unshift([1, 2, 3]);
+// console.log('array[0][1]=', array[0][1])
+// console.log('after adding 1, 2, 3 at beginning ', array);
+// // //adding to end
+// // array.push(10,20);
+// // console.log('after adding 20,30 at end', array);
+// // //adding to seredina
+// // array.splice(3,0,-10,-20);
+// // console.log('after adding -10 -20 at index 3', array);
+
+// iterations
+
+// array.forEach(function (value, index) {
+//     console.log(index + '. ' + value);
+// })
+
+// console.log('even numbers recived by filter', array.filter(function (value){
+//         return value % 2 === 0;
+// }
+// ).toString())
+
 console.log('-'.repeat(50));
-console.log('Adding operations');
-console.log('array before adding: ', array.toString());;
-//adding at beginning
-array.unshift([1, 2, 3]);
-console.log('array[0][1]=', array[0][1])
-console.log('after adding 1, 2, 3 at beginning ', array);
-// //adding to end
-// array.push(10,20);
-// console.log('after adding 20,30 at end', array);
-// //adding to seredina
-// array.splice(3,0,-10,-20);
-// console.log('after adding -10 -20 at index 3', array);
+console.log('array before ****: ', array.toString());;
+
+
+// var sum = 0;
+// array.forEach(function (value, index) {
+//     sum += value;
+// })
+//
+// console.log('sum: ', sum)
+
+// var sum = array.reduce(function (res, value) {
+//     return res + value;
+// },0)
+// console.log('sum: ', sum)
+
+var max = array.reduce(function (max, value) {
+    if (value > max) { return value; } else { return max;}
+})
+
+console.log('max: ', max)
+
 
