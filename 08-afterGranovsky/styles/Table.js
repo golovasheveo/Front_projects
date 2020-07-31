64e36925609354f2fadcfffa3883cdb3a79e0e5d
Evgeniy Golovashev <59750599+golovasheveo@users.noreply.github.com>
2020-07-31 18:28:26 +0300

    Add removeElement

--- 08-afterGranovsky/styles/Table.js
+++ 08-afterGranovsky/styles/Table.js
@@ -10,9 +10,12 @@ TableOfOrder.prototype.createHeader = function (headers) {
         return $('<th>', {
             text: header
         })
+
     })
-    this.$trHeader.append($thElements)
+    this.$trHeader.append($thElements).append($('<th>', {text: 'trash'}))
 }
+
+
 
 TableOfOrder.prototype.getHeader = function () {
     var headers = this.$headSelector.serializeArray()
@@ -36,7 +39,22 @@ TableOfOrder.prototype.addRows = function (objectToAddingRows) {
             text: objectToAddingRows[key]
         })
     })
-    this.$trBody.append($tdElement);
+
+    var $removeIcon = $('<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
+    '  <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>\n' +
+    '</svg>');
+
+    this.$trBody.append($trElement);
+    $trElement.append($tdElement)
+
+    var $tdRemove = $('<td>').append($removeIcon);
+
+   $trElement.append($tdRemove);
+
+    $tdRemove.on('click', function() {
+            $trElement.remove();
+            console.log('Row removed')
+        }.bind(this))
 }
 
 
